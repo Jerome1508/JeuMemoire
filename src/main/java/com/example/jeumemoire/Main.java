e5755349b29e8510184f3a929a92e5f70eddf953
Jerome1508 <jerome.chirol@hotmail.fr>
2023-03-03 16:44:31 +0100

    commit

--- src/main/java/com/example/jeumemoire/Main.java
+++ src/main/java/com/example/jeumemoire/Main.java
@@ -4,20 +4,66 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-public class HelloApplication extends Application {
+import javafx.scene.Group;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Pane;
+
+
+
+
+
+public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Jeu de mémoire!");
         stage.setScene(scene);
         stage.show();
+
+
+
+
     }
 
+
+    public static void letsgo(Stage stage) throws IOException {
+
+
+        InputStream stream = new FileInputStream("D:\\JeuMemoire\\target\\chat.jpg");
+        Image image = new Image(stream);
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+        imageView.setX(10);
+        imageView.setY(10);
+        imageView.setFitWidth(575);
+        imageView.setPreserveRatio(true);
+        Group root = new Group(imageView);
+        Scene scene1 = new Scene(root, 595, 370);
+        stage.setTitle("Displaying Image");
+        stage.setScene(scene1);
+        stage.show();
+
+    }
+
+
     public static void main(String[] args) {
+
+
         launch();
     }
 }
--- src/main/java/com/example/jeumemoire/MainController.java
+++ src/main/java/com/example/jeumemoire/MainController.java
@@ -1,14 +1,183 @@
 package com.example.jeumemoire;
 
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
+
+public class MainController {
+
+
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label Label1;
+
+    @FXML
+    private Label Label2;
+
+    @FXML
+    private Label Label3;
+
+    @FXML
+    private Label Label4;
+
+    @FXML
+    private Label Label5;
+
+    @FXML
+    private Label Label6;
+
+    @FXML
+    private Label Label7;
+
+    @FXML
+    private Label Label8;
+
+    @FXML
+    private Label Label;
+
+    @FXML
+    protected int Compter() {
+        int count = 0;
+        if (!Label1.getText().equals("")) {
+            count++;
+        }
+        if (!Label2.getText().equals("")) {
+            count++;
+        }
+        if (!Label3.getText().equals("")) {
+            count++;
+        }
+        if (!Label4.getText().equals("")) {
+            count++;
+        }
+        if (!Label5.getText().equals("")) {
+            count++;
+        }
+        if (!Label6.getText().equals("")) {
+            count++;
+        }
+        if (!Label7.getText().equals("")) {
+            count++;
+        }
+        if (!Label8.getText().equals("")) {
+            count++;
+        }
+        return count;
     }
+
+    private boolean Victoire() {
+        String[] labels = {Label1.getText(), Label2.getText(), Label3.getText(), Label4.getText(), Label5.getText(), Label6.getText(), Label7.getText(), Label8.getText()};
+
+        for (int i = 0; i < labels.length; i++) {
+            if (!labels[i].equals("")) {
+                for (int j = i + 1; j < labels.length; j++) {
+                    if (!labels[j].equals("") && labels[i].equals(labels[j])) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+    private void Cgagneoupas() {
+        if(Victoire() == true){
+            Label.setText("Victoire t'as gagné");
+        }
+    }
+    @FXML
+    protected void onButton1Click() {
+        if (Label1.getText().equals("Chat")) {
+            Label1.setText("");
+        } else if(Compter() < 2 ){
+            Label1.setText("Chat");
+        }
+        Cgagneoupas();
+    }
+
+
+
+    @FXML
+    protected void onButton2Click() {
+        if (Label2.getText().equals("Chien")) {
+            Label2.setText("");
+        } else if(Compter() < 2 ) {
+            Label2.setText("Chien");
+        }
+        Cgagneoupas();
+    }
+    @FXML
+    protected void onButton3Click() {
+        if (Label3.getText().equals("Chat")) {
+            Label3.setText("");
+        } else if(Compter() < 2 ) {
+            Label3.setText("Chat");
+        }
+        Cgagneoupas();
+    }
+
+    @FXML
+    protected void onButton4Click() {
+        if (Label4.getText().equals("Chien")) {
+            Label4.setText("");
+        } else if(Compter() < 2 ) {
+            Label4.setText("Chien");
+        }
+        Cgagneoupas();
+    }
+
+    @FXML
+    protected void onButton5Click() {
+            if (Label5.getText().equals("Rat")) {
+            Label5.setText("");
+        } else if(Compter() < 2 ) {
+            Label5.setText("Rat");
+        }
+        Cgagneoupas();
+    }
+
+    @FXML
+    protected void onButton6Click() {
+        if (Label6.getText().equals("Rat")) {
+            Label6.setText("");
+        } else if(Compter() < 2 ) {
+            Label6.setText("Rat");
+        }
+        Cgagneoupas();
+    }
+    @FXML
+    protected void onButton7Click() {
+        if (Label7.getText().equals("Pigeon")) {
+            Label7.setText("");
+        } else if(Compter() < 2 ) {
+            Label7.setText("Pigeon");
+        }
+        Cgagneoupas();
+    }
+
+    @FXML
+    protected void onButton8Click() {
+        if (Label8.getText().equals("Pigeon")) {
+            Label8.setText("");
+        } else if(Compter() < 2 ) {
+            Label8.setText("Pigeon");
+        }
+        Cgagneoupas();
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
